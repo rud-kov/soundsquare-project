fcba08f5cc75ec6c44c91adbbb7cc19377804ee7
Matous <matous.barnat@gmail.com>
2024-05-15 17:09:56 +0200

    import

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+// import _ from "lodash";
+
+import { debounce } from "lodash";
+
 const html = document.querySelector("html");
 const body = document.querySelector("body");
 
@@ -7,15 +11,36 @@ const Engine = {
 			// list of all functions to run on init
 			Engine.ui.events();
 			Engine.ui.forms();
+			Engine.ui.responsive();
 		},
 		events: function () {
 			// Add your events here
 			console.log("Events are running");
+
+			const el = document.querySelector(".b-test h1");
+			const offsetTop = Engine.helpers.offset(el);
+
+			console.log(offsetTop.top);
 		},
 		forms: function () {
 			// Add your form functions here
 			console.log("Forms are running");
 		},
+		responsive: function () {
+			// Add your responsive functions here
+			console.log("Responsive functions are running");
+
+			window.addEventListener(
+				"resize",
+				debounce(() => {
+					Engine.ui.recalculateGrid();
+				}, 1000)
+			);
+		},
+		recalculateGrid: debounce(function () {
+			// Add your grid functions here
+			console.log("Grid functions are running");
+		}, 100),
 	},
 	helpers: {
 		offset: function (el) {
